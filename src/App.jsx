591be7d5c2c051fe9d171cd2d1e0be506fcc6382
Birçe Yakut <yakutbirce@gmail.com>
2025-03-26 16:36:36 +0300

    Fixed Navbar import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'; 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
 import Skills from './components/SkillsSection';
 import Profile from './components/ProfileSection';
