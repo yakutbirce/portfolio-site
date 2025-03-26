50bfa2a10895fdc28d0e8e677ccd426de14abbe5
Birçe Yakut <yakutbirce@gmail.com>
2025-03-26 16:44:30 +0300

    fix navbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css'; 
-import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
 import Skills from './components/SkillsSection';
 import Profile from './components/ProfileSection';
 import Projects from './components/ProjectsSection';
 import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <Navbar />  
+      <NavBar />  
       <Hero />
       <Skills />
       <Profile  /> 
